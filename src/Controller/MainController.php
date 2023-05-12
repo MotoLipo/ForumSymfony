3b6414772bf95d58cabb1e33235cce9076417677
AndreyYashin <megadobro030@gmail.com>
2023-05-12 14:00:24 +0300

    install RabbitMQ

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -13,7 +13,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Predis;
 
 class MainController extends AbstractController
 {
