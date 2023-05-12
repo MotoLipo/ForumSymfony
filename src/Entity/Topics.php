0fdcd72881c176f9e42ff62de220d2dc7c134b26
AndreyYashin <megadobro030@gmail.com>
2023-05-12 16:56:34 +0300

    install API Platform and added API project

--- src/Entity/Topics.php
+++ src/Entity/Topics.php
@@ -2,37 +2,54 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
 use App\Repository\TopicsRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\String\Slugger\SluggerInterface;
 
 #[ORM\Entity(repositoryClass: TopicsRepository::class)]
 #[UniqueEntity('slug')]
 #[ORM\HasLifecycleCallbacks]
+#[ApiResource(
+    operations: [
+        new Get(normalizationContext: ['groups' => 'topics:item']),
+        new GetCollection(normalizationContext: ['groups' => 'topics:list'])
+    ],
+    order: ['id' => 'DESC'],
+    paginationEnabled: false,
+)]
 class Topics
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(['topics:list', 'topics:item'])]
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(['topics:list', 'topics:item'])]
     private ?string $name = null;
 
     #[ORM\OneToMany(mappedBy: 'topics', targetEntity: Comment::class, orphanRemoval: true)]
     private Collection $comments;
 
     #[ORM\Column(length: 255, unique: true)]
+    #[Groups(['topics:list', 'topics:item'])]
     private ?string $slug = null;
 
     #[ORM\Column(type: Types::TEXT)]
+    #[Groups(['topics:list', 'topics:item'])]
     private ?string $text = null;
 
     #[ORM\Column]
+    #[Groups(['topics:list', 'topics:item'])]
     private ?\DateTimeImmutable $createDate = null;
 
     #[ORM\PrePersist]
